ff9c66a719867f535dcaaa2b2c693b8ee8021682
Aayushman Sharma <aayushmansharma108@gmail.com>
2025-11-19 23:30:55 +0545

    feat: learing flip animation

--- src/components/heroSection/heroSection.component.tsx
+++ src/components/heroSection/heroSection.component.tsx
@@ -1,6 +1,7 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import "./heroSection.style.css";
+import { Flip } from "gsap/all";
 
 export function HeroSection() {
   useGSAP(() => {
@@ -12,29 +13,77 @@ export function HeroSection() {
         ease: "power1.inOut",
       })
       .to(".revealImage > img", {
-        scale: 1,
+        scale: 1.05,
         duration: 0.8,
-        ease: "power1.out",
+        ease: "power2.inOut",
       });
 
     const staggeredImagesTimeline = gsap.timeline();
 
-    const staggeredImages = document.querySelectorAll(".staggeredImages > img");
+    const staggeredImages = document.querySelectorAll<HTMLImageElement>(
+      ".staggeredImages > img"
+    );
 
     staggeredImages.forEach((img, index) => {
       gsap.set(img, {
         zIndex: index + 10,
       });
       staggeredImagesTimeline.to(img, {
-        stagger: 1,
+        stagger: 1.2,
         opacity: 1,
-        duration: 1,
-        scale: 1,
-        ease: "power1.out",
+        duration: 1.2,
+        scale: 1.05,
+        ease: "power2.inOut",
       });
     });
 
-    revealImageTimeline.add(staggeredImagesTimeline);
+    const revealImage =
+      document.querySelector<HTMLImageElement>(".revealImage > img");
+
+    const mainImages = document.querySelectorAll<HTMLImageElement>(
+      ".staggeredImages > img.mainImg"
+    );
+
+    const targets = document.querySelectorAll<HTMLElement>(
+      ".targetContainer > .target"
+    );
+
+    revealImageTimeline.add(staggeredImagesTimeline).add(() => {
+      staggeredImages.forEach((img) => {
+        if (!img.classList.contains("mainImg")) {
+          img.remove();
+        }
+      });
+      revealImage?.remove();
+      // Flip.fit(
+      //   ".staggeredImages > img.container",
+      //   ".targetContainer > .target",
+      //   {}
+      // );
+
+      const state = Flip.getState(mainImages);
+
+      mainImages.forEach((img, i) => {
+        targets[i].appendChild(img);
+      });
+
+      Flip.from(state, {
+        duration: 1.5,
+        ease: "power2.inOut",
+        stagger: -0.2,
+        scale: true,
+        simple: true,
+        onComplete: () => {
+          gsap.to(mainImages, {
+            borderRadius: "6px",
+            duration: 1,
+            ease: "power2.inOut",
+            stagger: 0.1,
+            overflow: "hidden",
+          });
+        },
+      });
+    });
   });
   return (
     <div className="heroSectionContainer relative w-screen h-screen">
@@ -45,10 +94,16 @@ export function HeroSection() {
         <img src="/assets/hero_2.jpg" alt="" />
         <img src="/assets/hero_3.jpg" alt="" />
         <img src="/assets/hero_4.jpg" alt="" />
-        <img src="/assets/hero_5.jpg" alt="" />
-        <img src="/assets/hero_6.jpg" alt="" />
-        <img src="/assets/hero_7.jpg" alt="" />
+        <img className="mainImg" src="/assets/hero_5.jpg" alt="" />
+        <img className="mainImg" src="/assets/hero_6.jpg" alt="" />
+        <img className="mainImg" src="/assets/hero_7.jpg" alt="" />
       </figure>
+
+      <div className="targetContainer">
+        <div className="target"></div>
+        <div className="target"></div>
+        <div className="target"></div>
+      </div>
     </div>
   );
 }
